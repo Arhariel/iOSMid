ab009f2cd9ca5a35a81aa52b72df04a3337b23ed
Arhariel <arharie@yandex.ru>
2025-03-17 14:44:35 +0500

    1

--- ul11/Task.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import FirebaseFirestore
-
-struct Task: Identifiable, Codable {
-    @DocumentID var id: String?
-    var title: String
-    var category: String
-    var note: String?
-}
